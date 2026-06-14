c0c6f188ba4958951eb7f012f1389048b6485515
Luiz Manoel <luizmanoelramalhosantos74@gmail.com>
2026-06-14 13:02:06 -0300

    Identifica extensões dos arquivos

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 pasta = Path(".")
 
-print("Arquivos encontrados:")
+print("Arquivos encontrados:\n")
 
 for arquivo in pasta.iterdir():
     if arquivo.is_file():
-        print(arquivo.name)
+        print(f"{arquivo.name} -> {arquivo.suffix}")
